ca1f96eb4c7405cb1b3ca30452345560da9a7c46
Richard Lo <richardjosonlo@gmail.com>
2018-06-20 23:00:58 +0100

    removed comment

--- check-value.js
+++ check-value.js
@@ -1,6 +1,5 @@
 module.exports.checkRow = function(board, row, value) {
   for (let i = 0; i < board[row].length; i++) {
-    // console.log(row + ',' + i + ':' + board[row][i]);
     if (board[row][i] == value) {
       return false;
     }
