261b445b6313940dd523b7f0d657a9f01abc2f35
Richard Lo <richardjosonlo@gmail.com>
2018-06-21 17:58:57 +0100

    added checkRow, checkCol, checkSquare, and checkValue

--- check-value-spec.js
+++ check-value-spec.js
@@ -2,7 +2,7 @@
 
 const Chai = require('chai');
 const expect = Chai.expect;
-const checkValue = require('./check-value.js');
+const solver = require('./check-value.js');
 
 let board = [
   [0, 9, 0, 0, 0, 0, 0, 0, 6],
@@ -16,28 +16,72 @@ let board = [
   [3, 0, 0, 0, 9, 0, 0, 0, 0],
 ];
 
-describe('checkValue', function() {
-    it('should exist', function() {
-        expect(checkValue).to.not.be.undefined;
-    });
+describe('#checkValue', function() {
+  it('should exist', function() {
+      expect(solver).to.not.be.undefined;
+  });
 
-    it('should check that each value in the row ' +
-      'does not equal the input value', function() {
+  it('should check if value is valid', function() {
+    let checkValue;
+
+    // No match. Return true.
+    checkValue = solver.checkValue(board, 2, 3, 7);
+    expect(checkValue).to.be.true;
+
+    // Match found. Return false.
+    checkValue = solver.checkValue(board, 4, 3, 2);
+    expect(checkValue).to.be.false;
+
+    // Match found. Return false.
+    checkValue = solver.checkValue(board, 6, 4, 9);
+    expect(checkValue).to.be.false;
+
+    // No match. Return true.
+    checkValue = solver.checkValue(board, 3, 5, 7);
+    expect(checkValue).to.be.true;
+  });
+});
+
+describe('#checkRow', function() {
+  it('should check that each value in the row ' +
+    'does not equal the input value', function() {
+    let checkRow;
+    // No match. Return true.
+    checkRow = solver.checkRow(board, 0, 2);
+    expect(checkRow).to.be.true;
+    // Match found. Return false.
+    checkRow = solver.checkRow(board, 2, 8);
+    expect(checkRow).to.be.false;
+  });
+});
+
+describe('#checkCol', function() {
+  it('should check that each value in the column ' +
+    'does not equal the input value', function() {
+      let checkCol;
       // No match. Return true.
-      let checkRow = checkValue.checkRow(board, 0, 2);
-      expect(checkRow).to.be.true;
+      checkCol = solver.checkCol(board, 0, 8);
+      expect(checkCol).to.be.true;
       // Match found. Return false.
-      checkRow = checkValue.checkRow(board, 2, 8);
-      expect(checkRow).to.be.false;
+      checkCol = solver.checkCol(board, 2, 4);
+      expect(checkCol).to.be.false;
     });
+});
 
-    it('should check that each value in the column ' +
-      'does not equal the input value', function() {
-        // No match. Return true.
-        let checkCol = checkValue.checkCol(board, 0, 8);
-        expect(checkCol).to.be.true;
-        // Match found. Return false.
-        checkCol = checkValue.checkCol(board, 2, 4);
-        expect(checkCol).to.be.false;
-      });
+describe('#checkSquare', function() {
+  it('should check that each value in 3x3 square ' +
+    'does not equal the input value', function() {
+      let checkSquare;
+      // No match. Return true.
+      checkSquare = solver.checkSquare(board, 0, 0, 5);
+      expect(checkSquare).to.be.true;
+
+      // Match found. Return false.
+      checkSquare = solver.checkSquare(board, 8, 3, 4);
+      expect(checkSquare).to.be.false;
+
+      // Match found. Return false.
+      checkSquare = solver.checkSquare(board, 3, 4, 2);
+      expect(checkSquare).to.be.false;
+    });
 });
--- check-value.js
+++ check-value.js
@@ -1,3 +1,13 @@
+module.exports.checkValue = function(board, row, col, value) {
+  if (this.checkRow(board, row, value) &&
+    this.checkCol(board, col, value) &&
+    this.checkSquare(board, row, col, value)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 module.exports.checkRow = function(board, row, value) {
   for (let i = 0; i < board.length; i++) {
     if (board[row][i] == value) {
@@ -13,5 +23,36 @@ module.exports.checkCol = function(board, col, value) {
       return false;
     }
   }
+  return true;
+};
+
+module.exports.checkSquare = function(board, row, col, value) {
+  const squareSize = 3;
+  // Get origin of 3x3 square that holds a spot on the board.
+  let squareOrigin = [];
+  if (row >= 0 && row <= 2) {
+    squareOrigin[0] = 0;
+  } else if (row >= 3 && row <= 5) {
+    squareOrigin[0] = 3;
+  } else if (row >= 6 && row <= 8) {
+    squareOrigin[0] = 6;
+  }
+  if (col >= 0 && col <= 2) {
+    squareOrigin[1] = 0;
+  } else if (col >= 3 && col <= 5) {
+    squareOrigin[1] = 3;
+  } else if (col >= 6 && col <= 8) {
+    squareOrigin[1] = 6;
+  }
+
+  // Check values in 3x3 square to see if input value exists.
+  for (let i = squareOrigin[0]; i < squareOrigin[0] + squareSize; i++) {
+    for (let j = squareOrigin[1]; j < squareOrigin[1] + squareSize; j++) {
+      if (board[i][j] == value) {
+        return false;
+      }
+    }
+  }
+
   return true;
 };
